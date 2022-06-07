47f87922294629ad68990a5b0eddffa22fae36f5
steven <81638170+ssuter6@users.noreply.github.com>
2022-06-07 13:21:19 +0200

    Ajout bar chart pour fonction d3 + modification mise en page

--- map.js
+++ map.js
@@ -259,6 +259,65 @@ const button_communes = $("#Limite_communales").click(function(){
 });
 
 
+///////////////////////////////Création graphique Population par canton////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// dimensions et marges du graphique
+var margin = {top: -10, right:20, bottom:85, left:60},
+    width = 585 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
+
+// intervalles
+var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+var y = d3.scaleLinear()
+          .range([height, 0]);
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+
+var svg = d3.select("#bar").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+	.append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+d3.csv("canton_pop.csv").then(function(data) {
+
+  // format the data
+  data.forEach(function(d) {
+    d.Population = +d.Population;
+  });
+
+  // Scale the range of the data in the domains
+  x.domain(data.map(function(d) { return d.Canton; }));
+  y.domain([0, d3.max(data, function(d) { return d.Population; })]);
+
+  // append the rectangles for the bar chart
+  svg.selectAll(".bar_chart")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar_chart")
+      .attr("x", function(d) { return x(d.Canton); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.Population); })
+      .attr("height", function(d) { return height - y(d.Population); });
+
+  // add the x Axis
+  svg.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
 
 
+  // add the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
+});
